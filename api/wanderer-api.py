54e007dcdcb3e47e7bf8cbef16041def22443054
Sayak B <sayak-brm@users.noreply.github.com>
2020-01-31 19:35:47 +0530

    Revert "Merge branch 'development'"
    This reverts commit 68cf5df2413954b105d4688c04235fd60cf9198d, reversing
    changes made to 03903f18f631c8cae60aba009624ec7e213442a3.

--- api/wanderer-api.py
+++ api/wanderer-api.py
@@ -1,6 +1,4 @@
 #! /usr/bin/python3
-
-import json
 from flask import Flask
 from flask_restful import Api
 from flask_restful import Resource
@@ -11,45 +9,75 @@ api = Api(app)
 i2c = I2CIO(0x01)
 app.config["DEBUG"] = True
 
-opts = ''
-with open('params.json') as params:
-    opts = json.loads(params.read())
+relay_pins = {"DRL": 37, "DIP": 39}
+
 
 class Drive(Resource):
     def post(self, direction):
-        if direction in opts["dirs"]:
-            i2c.send_write([6,  opts["dirs"][direction]])
-            return {"success": True}
-        print("ERR: Incorrect Drive Direction")
-        return {"success": False}
+        if direction == "front":
+            i2c.send_write([6, 1])
+        elif direction == "back":
+            i2c.send_write([6, 2])
+        elif direction == "left":
+            i2c.send_write([6, 3])
+        elif direction == "right":
+            i2c.send_write([6, 4])
+        elif direction == "stop":
+            i2c.send_write([6, 5])
+        else:
+            print("ERR: Incorrect Drive Direction")
+            return {"success": False}
+        return {"success": True}
 
 
 class Sonar(Resource):
     def get(self):
         i2c.send_write([0])
+        print("Sonar Requested")
         return i2c.send_read(3)
 
 
 class Lights(Resource):
+    @staticmethod
+    def drl(state):
+        if state == "on":
+            i2c.send_write([2, relay_pins["DRL"], 0])
+        if state == "off":
+            i2c.send_write([2, relay_pins["DRL"], 1])
+
+    @staticmethod
+    def dip(state):
+        if state == "on":
+            i2c.send_write([2, relay_pins["DIP"], 0])
+        if state == "off":
+            i2c.send_write([2, relay_pins["DIP"], 1])
+
     def post(self, light, state):
-        if light in opts["lights"]:
-            if state in opts["relay_ao"]:
-                i2c.send_write([2, opts["lights"][light],
-                    opts["relay_ao"][state]])
-                return {"success": True}
-        return {"success": False}
+        if light == "drl":
+            Lights.drl(state)
+        if light == "dip":
+            Lights.dip(state)
 
-class Gears(Resource):
+
+class Gear(Resource):
     def post(self, number):
-        if number in opts["gears"]:
-            i2c.send_write([7,  opts["gears"][number]])
-            return {"success": True}
-        return {"success": False}
+        if number == "1":
+            i2c.send_write([7, 63])
+        elif number == "2":
+            i2c.send_write([7, 127])
+        elif number == "3":
+            i2c.send_write([7, 191])
+        elif number == "4":
+            i2c.send_write([7, 255])
+        else:
+            return {"success": False}
+        return {"success": True}
 
-api.add_resource(Drive, '/api/drive/<direction>')
-api.add_resource(Sonar, '/api/sonar/all')
-api.add_resource(Lights, '/api/lights/<light>/<state>')
-api.add_resource(Gears, '/api/gear/<number>')
+
+api.add_resource(Drive, "/api/drive/<direction>")
+api.add_resource(Sonar, "/api/sonar/all")
+api.add_resource(Lights, "/api/lights/<light>/<state>")
+api.add_resource(Gear, "/api/gear/<number>")
 
 if __name__ == "__main__":
     app.run(port="8500", host="0.0.0.0")
