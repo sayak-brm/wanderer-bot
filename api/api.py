1d7d279bf93365cbe14c295579c115e0bb32b7ad
Restyled.io <commits@restyled.io>
2020-02-03 05:55:04 +0000

    Restyled by black

--- api/api.py
+++ api/api.py
@@ -10,8 +10,8 @@ API = Api(APP)
 I2C = I2CIO(0x01)
 APP.config["DEBUG"] = True
 
-opts = ''
-with open('params.json') as params:
+opts = ""
+with open("params.json") as params:
     opts = json.loads(params.read())
 
 
@@ -19,7 +19,7 @@ class Drive(Resource):
     @staticmethod
     def post(direction):
         if direction in opts["dirs"]:
-            I2C.send_write([6,  opts["dirs"][direction]])
+            I2C.send_write([6, opts["dirs"][direction]])
             return {"success": True}
         print("ERR: Incorrect Drive Direction")
         return {"success": False}
@@ -37,8 +37,7 @@ class Lights(Resource):
     def post(light, state):
         if light in opts["lights"]:
             if state in opts["relay_ao"]:
-                I2C.send_write([2, opts["lights"][light],
-                                opts["relay_ao"][state]])
+                I2C.send_write([2, opts["lights"][light], opts["relay_ao"][state]])
                 return {"success": True}
         return {"success": False}
 
@@ -47,15 +46,15 @@ class Gears(Resource):
     @staticmethod
     def post(number):
         if number in opts["gears"]:
-            I2C.send_write([7,  opts["gears"][number]])
+            I2C.send_write([7, opts["gears"][number]])
             return {"success": True}
         return {"success": False}
 
 
-API.add_resource(Drive, '/api/drive/<direction>')
-API.add_resource(Sonar, '/api/sonar/all')
-API.add_resource(Lights, '/api/lights/<light>/<state>')
-API.add_resource(Gears, '/api/gear/<number>')
+API.add_resource(Drive, "/api/drive/<direction>")
+API.add_resource(Sonar, "/api/sonar/all")
+API.add_resource(Lights, "/api/lights/<light>/<state>")
+API.add_resource(Gears, "/api/gear/<number>")
 
 if __name__ == "__main__":
     APP.run(port="8500", host="0.0.0.0")
